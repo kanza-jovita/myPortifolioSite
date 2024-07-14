075dbb7e80614dba6c240457037db4f1f7a8b911
Kanza Jovita <kanzajovita@gmail.com>
2024-07-14 16:40:26 +0300

    best site

---
+++ Deel/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# from .models import *
+# from .forms import *
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'Deel/index.html')
